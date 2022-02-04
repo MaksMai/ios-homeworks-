393cc0ae3c1c73b24d8e3544b5941b748cbb1b58
Maksim Maiorov <mvmayorov@icloud.com>
2022-02-04 11:27:32 +0300

    Домашнее задани №1.2

---
+++ Navigation/ProfileView.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileView.swift
+//  Navigation
+//
+//  Created by Maksim Maiorov on 04.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    // фото пользователя
+    @IBOutlet weak var userPhoto: UIImageView!
+    // имя пользователя
+    @IBOutlet weak var userName: UILabel!
+    // информация о дне рождения
+    @IBOutlet weak var birthdayInformation: UILabel!
+    // информация о месте нахождения (город)
+    @IBOutlet weak var locationInformation: UILabel!
+    // место для подписи
+    @IBOutlet weak var placeForSignature: UITextView!
+
+}
