f1cf43a053f13d0e6cc88b18dfb04d79fd83902f
Maksim Maiorov <mvmayorov@icloud.com>
2022-02-07 09:29:30 +0300

    Подчищен после проверки

--- Navigation/ProfileView.swift
+++ Navigation/ProfileView.swift
@@ -20,28 +20,4 @@ class ProfileView: UIView {
     @IBOutlet weak var placeForSignature: UITextView!
     
     
-//    ДАННЫЙ БЛОК НЕ ПОШЕЛ
-//    override init(frame: CGRect) {
-//            super.init(frame: frame)
-//            self.setupView()
-//        }
-//        
-//        required init?(coder: NSCoder) {
-//            super.init(coder: coder)
-//            self.setupView()
-//        }
-//        
-//        private func setupView() {
-//            let view = self.loadViewFromXib()
-//            view.frame = self.bounds
-//            view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//            self.addSubview(view)
-//        }
-//        
-//        private func loadViewFromXib() -> UIView {
-//            guard let view = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? UIView else { return UIView() }
-//            
-//            return view
-//        }
-
 }
